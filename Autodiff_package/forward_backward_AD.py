099218fe112c093a63dc1cc2aee0082374e07996
Ruben Dragland <ruben.s.dragland@gmail.com>
2022-10-19 15:53:01 +0200

    Debugging progress

--- Autodiff_package/forward_backward_AD.py
+++ Autodiff_package/forward_backward_AD.py
@@ -36,8 +36,30 @@ def main(
     """
     Main function.
     """
-    theta_struct_it, phi_struct_it, a_temp_it = format_matlab_input(
-        theta_struct_it, phi_struct_it, a_temp_it
+    (
+        theta_struct_it,
+        phi_struct_it,
+        a_temp_it,
+        Ylm_coef,
+        ny,
+        nx,
+        nz,
+        numOfsegments,
+        numOfpixels,
+        numOfCoeffs,
+        numOfvoxels,
+    ) = format_matlab_input(
+        theta_struct_it,
+        phi_struct_it,
+        a_temp_it,
+        Ylm_coef,
+        ny,
+        nx,
+        nz,
+        numOfsegments,
+        numOfpixels,
+        numOfCoeffs,
+        numOfvoxels,
     )
 
     error_norm, AD_grad_coeff, AD_grad_theta, AD_grad_phi = SAXSTT_forward_backward(
@@ -78,21 +100,55 @@ def main(
 
 
 def format_matlab_input(
-    theta_struct_it, phi_struct_it, a_temp_it
+    theta_struct_it,
+    phi_struct_it,
+    a_temp_it,
+    Ylm_coef,
+    ny,
+    nx,
+    nz,
+    numOfsegments,
+    numOfpixels,
+    numOfCoeffs,
+    numOfvoxels,
 ):  # , ny, nx, nz, numOfsegments, current_projection, p, X, Y, Z, numOfpixels, unit_q_beamline, Ylm_coef, find_coefficients, find_orientation, numOfCoeffs, numOfvoxels):
     """
     Deals with matlab engine things.
     After this, assume everything is in torch.
     """
-
+    logging.debug("Before a_temp_it shape: {}".format(a_temp_it.size))
+    # RSD: Unsure on effect of reshape. Might be able to remove. Yes can be removed.
     theta_struct_it = torch.tensor(theta_struct_it.reshape(theta_struct_it.size))
     phi_struct_it = torch.tensor(phi_struct_it.reshape(phi_struct_it.size))
     a_temp_it = torch.tensor(a_temp_it.reshape(a_temp_it.size))
+    Ylm_coef = torch.tensor(Ylm_coef.reshape(Ylm_coef.size))
+
+    logging.debug("After a_temp_it shape: {}".format(a_temp_it.shape))
 
     # Add more transforms as needed.
     # Add possible all conversion of numbers
 
-    return theta_struct_it, phi_struct_it, a_temp_it
+    ny = int(np.squeeze(ny))
+    nx = int(np.squeeze(nx))
+    nz = int(np.squeeze(nz))
+    numOfsegments = int(np.squeeze(numOfsegments))
+    numOfpixels = int(np.squeeze(numOfpixels))
+    numOfCoeffs = int(np.squeeze(numOfCoeffs))
+    numOfvoxels = int(np.squeeze(numOfvoxels))
+
+    return (
+        theta_struct_it,
+        phi_struct_it,
+        a_temp_it,
+        Ylm_coef,
+        ny,
+        nx,
+        nz,
+        numOfsegments,
+        numOfpixels,
+        numOfCoeffs,
+        numOfvoxels,
+    )
 
 
 def SAXSTT_forward_backward(
@@ -131,6 +187,8 @@ def SAXSTT_forward_backward(
     if find_orientation:
         theta_struct_it.requires_grad_(True).to(device)
         phi_struct_it.requires_grad_(True).to(device)
+
+    # RSD: Might have to do to.device for all tensors. data, etc. Current assumption is that it is most beneficial to only upload the tensors that are differentiated
 
     error_norm, AD_grad_coeff, AD_grad_theta, AD_grad_phi = SAXS_AD_cost_function(
         theta_struct_it,
@@ -178,12 +236,11 @@ def SAXS_AD_cost_function(
     numOfCoeffs,
     numOfvoxels,
 ):
+    # RSD: Want data as 3D tensor. Therefore no reshape.
+    data = torch.tensor(current_projection["data"])
 
-    data = current_projection["data"]
     Rot_exp_now = current_projection["Rot_exp"]
     unit_q_object = torch.tensor(Rot_exp_now @ unit_q_beamline)
-
-    numOfvoxels = int(np.squeeze(numOfvoxels))
 
     sin_theta_struct = torch.reshape(torch.sin(theta_struct), (1, 1, numOfvoxels))
     cos_theta_struct = torch.reshape(torch.cos(theta_struct), (1, 1, numOfvoxels))
@@ -226,44 +283,57 @@ def SAXS_AD_cost_function(
     logging.debug("unit_q_object shape: {}".format(unit_q_object.shape))
     # Rot_str = torch.reshape(Rot_str, (3, 3, numOfvoxels))  # Should it be like this?
 
-    q_pp = torch.matmul(
-        Rot_str, unit_q_object
-    )  # RSD: Figure out basxpagemult to manage this.
-    cos_theta_sh_cut = q_pp[3, :, :]
+    q_pp = page_multiply(Rot_str, unit_q_object)
+    # RSD: Old did not work: torch.matmul( #Rot_str, unit_q_object #)
+    # RSD: Retrieve last index of q_pp
+    cos_theta_sh_cut = q_pp[-1, :, :]
     logging.debug("cos_theta_sh_cut shape: {}".format(cos_theta_sh_cut.shape))
 
     block_cos_theta_powers = repmat_cumprod_SH(
         ones_struct, cos_theta_sh_cut, numOfCoeffs
     )
+
+    logging.debug("Ylm_coef: {}".format(Ylm_coef))
     logging.debug(
         "block_cos_theta_powers shape: {}".format(block_cos_theta_powers.shape)
     )
-
     Ylm = page_multiply(Ylm_coef, block_cos_theta_powers)
     logging.debug("Ylm shape: {}".format(Ylm.shape))
 
+    logging.debug("a_temp shape: {}".format(a_temp.shape))
     sumlm_alm_Ylm = page_multiply(a_temp, Ylm)
     logging.debug("sumlm_alm_Ylm shape: {}".format(sumlm_alm_Ylm.shape))
 
-    data_synt_vol = torch.permute(torch.abs(sumlm_alm_Ylm**2), (3, 2, 1))
+    # RSD: Python vs Matlab indexing
+    data_synt_vol = torch.permute(torch.abs(sumlm_alm_Ylm**2), (2, 1, 0))
     data_synt_vol = torch.reshape(data_synt_vol, (ny, nx, nz, numOfsegments))
 
+    logging.debug("data_synt_vol shape: {}".format(data_synt_vol.shape))
+
+    logging.debug("Data type, shape: {}, {}".format(data.dtype, data.shape))
+
     xout = (
-        np.arange(1, data.size(1))
-        - torch.ceil(data.size(1) / 2)
-        + current_projection["dx"]
-    )  # RSD: NB indices. Are they correct?
-    yout = (
-        np.arange(1, data.size(0))
-        - torch.ceil(data.size(0) / 2)
-        + current_projection["dy"]
+        np.arange(1, data.size(1) + 1)
+        - np.ceil(data.size(1) / 2)
+        + np.squeeze(current_projection["dx"])  # RSD: or [0]
     )
+    # RSD: NB MATLAB vs Python indices
+    yout = (
+        np.arange(1, data.size(0) + 1)
+        - np.ceil(data.size(0) / 2)
+        + np.squeeze(current_projection["dy"])  # [0]
+    )
+
+    logging.debug("xout shape: {}".format(xout.shape))
+    logging.debug("yout shape: {}".format(yout.shape))
+    logging.debug(f"\nxout: {xout}\nyout: {yout}")
 
     proj_out_all = arb_projection(data_synt_vol, X, Y, Z, Rot_exp_now, p, xout, yout)
 
     aux_diff_poisson = (
         torch.sqrt(proj_out_all) - torch.sqrt(data)
     ) * current_projection["window_mask"]
+    # RSD: Check shape np.squeeze?
 
     error_norm = (
         2 * torch.sum(aux_diff_poisson**2, dim=(2, 1, 0)) / numOfpixels
@@ -291,35 +361,43 @@ def SAXS_AD_cost_function(
 def page_multiply(A: torch.tensor, B: torch.tensor):
     # Use Einstein sum in different cases
 
-    assert len(A.size()) == 3, "A is not a 3D tensor"
-    assert len(B.size()) <= len(A.size()), "B is not a 3D or 2D tensor"
+    # assert len(A.size()) == 3, "A is not a 3D tensor"
+    # assert len(B.size()) <= len(A.size()), "B is not a 3D or 2D tensor"
 
-    if A.size()[-1] == B.size()[-1] and len(B.size()) == 3:
+    logging.debug("A shape: {}".format(A.shape))
+    logging.debug("B shape: {}".format(B.shape))
 
+    if A.shape[-1] == B.shape[-1] and len(B.shape) == 3:
+
+        if len(A.shape) == 1:
+            A = A.unsqueeze(0).unsqueeze(0)  # RSD: Add two dimensions
         C = torch.einsum("ijm,jkm->ikm", A, B)
-    elif len(A.size()) > len(B.size()):
+
+    elif len(A.shape) > len(B.shape):
 
         C = torch.einsum("ijk,jm->imk", A, B)
+
+    elif len(A.shape) < len(B.shape) and len(B.shape) == 3:
+
+        if len(A.shape) == 1:
+            A = A.unsqueeze(0)  # RSD: Add batch dimension
+
+        logging.debug("A dims: {}".format(len(A.shape)))
+        logging.debug("B dims: {}".format(len(B.shape)))
+
+        C = torch.einsum("ij,jkm->ikm", A, B)  # RSD: Works for a_temp as well?
     else:
         raise ValueError(
             "A and B have incompatible shapes. Please implement desired einsum"
         )
-    """
-    Does not work, but should work in one case
-    assert A.size[-1] == B.size[-1]
-
-    for i in range(A.size[-1]):
-
-        A[:, :, i] = torch.matmul(
-            A[:, :, i], B[:, :, i]
-        )  # Check if works compared to creating new Tensor.
-    """
     return C
 
 
 def repmat_cumprod_SH(ones_struct, cos_theta_sh_cut, numOfCoeffs):
-
-    copy_matrix = torch.ones((numOfCoeffs, ones_struct.size(1), ones_struct.size(2)))
+    # RSD: This is a bit of a hack. Should be possible to do it with einsum. AI says...
+    copy_matrix = torch.ones(
+        (numOfCoeffs, cos_theta_sh_cut.shape[-2], cos_theta_sh_cut.shape[-1])
+    )
     # RSD: Difference from original: Create entire matrix, iterate from 1.
 
     for i in range(1, numOfCoeffs):
@@ -353,9 +431,13 @@ def arb_projection(tomo_obj_all, X, Y, Z, R, p, xout, yout):  # Assume numpy
     ) - Ax  # Variable from 0 to 1 from x distance of pixel Ax to Ax+1 where the voxel hits
     Ty = (Yp - min_yout + 1) - Ay
 
-    proj_out_all = torch.zeros(len(yout), len(xout), tomo_obj_all, requires_grad=True)
+    proj_out_all = torch.zeros(
+        len(yout), len(xout), tomo_obj_all.shape[-1], requires_grad=True
+    )
 
-    nRows, nCols, nPages = tomo_obj_all.size()
+    logging.debug("proj_out_all shape: {}".format(proj_out_all.shape))
+    logging.debug("tomo_obj_all shape: {}".format(tomo_obj_all.shape))
+    nRows, nCols, nPages = proj_out_all.size()
     page_out = nRows * nCols
     page_in = Ax.shape[0] * np.product(Ax.shape[1:])
     nElements = np.product(Ax.shape)
@@ -374,6 +456,20 @@ def arb_projection(tomo_obj_all, X, Y, Z, R, p, xout, yout):  # Assume numpy
         nRows,
         nCols,
     )
+    # proj_out_all = loop_interpolate(
+    #     proj_out_all,
+    #     tomo_obj_all,
+    #     Ax,
+    #     Ay,
+    #     Tx,
+    #     Ty,
+    #     page_in,
+    #     page_out,
+    #     nElements,
+    #     nPages,
+    #     nRows,
+    #     nCols,
+    # )
 
     if p["filter_2D"] == 0:
         pass
@@ -418,6 +514,7 @@ def loop_interpolate(
         nElements
     ):  # Try to use numba here. Or use torch functions. Or use torch functions with numba. Or array operations.
 
+        # RSD: Matlab implementation. Will not work
         if (Ax[i] > 0) and (Ax[i] < nCols) and (Ay[i] > 0) and (Ay[i] < nRows):
             # Check dimensions of Ax.
 
@@ -454,6 +551,9 @@ def array_interpolate(
     nPages,
 ):
     """Interpolation with array operations to avoid for loop."""
+
+    logging.debug("Ax shape: {}".format(Ax.shape))
+    logging.debug("Ax: {}".format(Ax))
     indices = np.argwhere((Ax > 0) and (Ax < nCols) and (Ay > 0) and (Ay < nRows))
 
     ind = Ay[indices] + nRows * Ax[indices]
